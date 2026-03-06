a6ffe70838f440c1465a72c42b11c3e1679630ff
guischuhcardosodev-coder <guischuhcardoso.dev@gmail.com>
2026-03-06 10:41:05 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,8 @@ import Home from "./pages/Home";
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/404"} component={NotFound} />
+      <Route path="/" component={Home} />
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
